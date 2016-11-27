8aa7775177c9ad958797294735bf17160ca78e05
Nat <nat@cmmc.io>
2016-11-28 01:17:01 +0700

    Refactor.

--- ManagerNTPClient/ManagerNTPClient.ino
+++ ManagerNTPClient/ManagerNTPClient.ino
@@ -6,6 +6,10 @@
 #include <WiFiUdp.h>
 #include <Ticker.h>
 
+/*
+ * UDP Header
+ */
+
 Ticker ntpTicker;
 bool shouldReloadNTPTime = true;
 bool isNTPReloaderLocked = false;
@@ -26,6 +30,13 @@ byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing pack
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP udp;
 
+/*
+* CURRENT
+*/
+
+#define LED_vcc   4
+#define LED_gnd   5
+
 void setup()
 {
   Serial.begin(115200);
@@ -44,7 +55,7 @@ void setup()
   Serial.println(udp.localPort());
 
 
-  delay(2000);
+  // ARM NTP Update Timer
   ntpTicker.attach(60000, []() {
     if (isNTPReloaderLocked) return;
     Serial.println("================");
@@ -52,6 +63,8 @@ void setup()
     Serial.println("================");
     shouldReloadNTPTime = true;
   });
+
+  delay(2000);
 }
 
 void loop()
@@ -80,51 +93,32 @@ void getNTPTask() {
       }
     }
     else {
-      Serial.print("packet received, length=");
-      Serial.println(cb);
-      // We'v e received a packet, read the data from it
-      udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+        Serial.print("packet received, length=");
+        Serial.println(cb);
+        // We'v e received a packet, read the data from it
+        udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
 
-      //the timestamp starts at byte 40 of the received packet and is four bytes,
-      // or two words, long. First, esxtract the two words:
+        //the timestamp starts at byte 40 of the received packet and is four bytes,
+        // or two words, long. First, esxtract the two words:
+        unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+        unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+        // combine the four bytes (two words) into a long integer
+        // this is NTP time (seconds since Jan 1 1900):
+        unsigned long secsSince1900 = highWord << 16 | lowWord;
+        // Serial.print("Seconds since Jan 1 1900 = " );
+        // Serial.println(secsSince1900);
 
-      unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-      unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-      // combine the four bytes (two words) into a long integer
-      // this is NTP time (seconds since Jan 1 1900):
-      unsigned long secsSince1900 = highWord << 16 | lowWord;
-      Serial.print("Seconds since Jan 1 1900 = " );
-      Serial.println(secsSince1900);
+        const unsigned long seventyYears = 2208988800UL;
+        // subtract seventy years:
+        unsigned long epoch = secsSince1900 - seventyYears;
+        // print Unix time:
 
-      // now convert NTP time into everyday time:
-      Serial.print("Unix time = ");
-      // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-      const unsigned long seventyYears = 2208988800UL;
-      // subtract seventy years:
-      unsigned long epoch = secsSince1900 - seventyYears;
-      // print Unix time:
-      Serial.println(epoch);
-
-
-      // print the hour, minute and second:
-      Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-      Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-      Serial.print(':');
-      if ( ((epoch % 3600) / 60) < 10 ) {
-        // In the first 10 minutes of each hour, we'll want a leading '0'
-        Serial.print('0');
+        Serial.printf("EPOCH = %lu \r\n", epoch);
+        Serial.printf("SECONDS = %lu \r\n", epoch % 60); // print the second
+        Serial.println("UNLOCK");
+        isNTPReloaderLocked = false;
+        break;
       }
-      Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-      Serial.print(':');
-      if ( (epoch % 60) < 10 ) {
-        // In the first 10 seconds of each minute, we'll want a leading '0'
-        Serial.print('0');
-      }
-      Serial.println(epoch % 60); // print the second
-      Serial.println("EXIT GET TIME");
-      isNTPReloaderLocked = false;
-      break;
-    }
   }
 }
 // send an NTP request to the time server at the given address
